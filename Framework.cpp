146899b7d9a2c952bda7ca1c1c31e7c34520219a
CorinneBrucks <brucksc@onid.oregonstate.edu>
2016-01-06 20:13:29 -0800

    Refactored Main and InitGraphics into Messy Framework

--- Framework.cpp
+++ Framework.cpp
@@ -9,14 +9,25 @@
 #include "glut.h"
 #include "glui.h"
 
-//If we need too we will make this a singleton
-Framework::Framework(/*int argc, char ** argv*/)
-{
-	
-	//Do the all Init Glui Stuff Here!
-	//glutInit(&argc, argv);
-	//Do all of the Init Glut Stuff Here
+int MainWindow;
+const int INIT_WINDOW_SIZE = { 600 };
+const char *WINDOWTITLE = { "OpenGL / GLUT / GLUI Sample -- Corinne Brucks" };
 
+// window background color (rgba):
+const float BACKCOLOR[] = { 0.1f, 0.1f, 0.1f, 0.f };
+const char *GLUITITLE = { "User Interface Window" };
+const float BOXSIZE = { 2.f };
+
+
+//If we need too we will make this a singleton
+Framework::Framework(int argc, char ** argv)
+{
+	glutInit(&argc, argv);
+	frameArgc = argc;
+	frameArgv = argv;
+	BuildClasses();
+	InitGraphics();
+	
 }
 
 
@@ -26,9 +37,103 @@ Framework::~Framework()
 }
 void Framework::Run(void) {
 
-}
+	//Do the all Init Glui Stuff Here!
 
-int main() {
-	//just here for compile checks. Remove when you want things to run
-	return 1;
+
+	//Do all of the Init Glut Stuff Here
+	RestoreDefaults();
+
+
+	// setup all the user interface stuff:
+
+	InitGlui();
+
+
+	// draw the scene once and wait for some interaction:
+	// (this will never return)
+
+	glutMainLoop();
+
+
+	// this is here to make the compiler happy:
+
+	return;
+}
+void Framework::BuildClasses() {
+
+}
+void Framework::RestoreDefaults() {
+
+}
+void Framework::InitGraphics() {
+	// setup the display mode:
+	// ( *must* be done before call to glutCreateWindow( ) )
+	// ask for color, double-buffering, and z-buffering:
+
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+
+
+	// set the initial window configuration:
+
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(INIT_WINDOW_SIZE, INIT_WINDOW_SIZE);
+
+
+	// open the window and set its title:
+
+	MainWindow = glutCreateWindow(WINDOWTITLE);
+	glutSetWindowTitle(WINDOWTITLE);
+
+	// setup the clear values:
+
+	glClearColor(BACKCOLOR[0], BACKCOLOR[1], BACKCOLOR[2], BACKCOLOR[3]);
+
+
+	// setup the callback routines:
+	// DisplayFunc -- redraw the window
+	// ReshapeFunc -- handle the user resizing the window
+	// KeyboardFunc -- handle a keyboard input
+	// MouseFunc -- handle the mouse button going down or up
+	// MotionFunc -- handle the mouse moving with a button down
+	// PassiveMotionFunc -- handle the mouse moving with a button up
+	// VisibilityFunc -- handle a change in window visibility
+	// EntryFunc	-- handle the cursor entering or leaving the window
+	// SpecialFunc -- handle special keys on the keyboard
+	// SpaceballMotionFunc -- handle spaceball translation
+	// SpaceballRotateFunc -- handle spaceball rotation
+	// SpaceballButtonFunc -- handle spaceball button hits
+	// ButtonBoxFunc -- handle button box hits
+	// DialsFunc -- handle dial rotations
+	// TabletMotionFunc -- handle digitizing tablet motion
+	// TabletButtonFunc -- handle digitizing tablet button hits
+	// MenuStateFunc -- declare when a pop-up menu is in use
+	// TimerFunc -- trigger something to happen a certain time from now
+	// IdleFunc -- what to do when nothing else is going on
+
+	glutSetWindow(MainWindow);
+	/*glutDisplayFunc(Display);
+	glutReshapeFunc(Resize);
+	glutKeyboardFunc(Keyboard);
+	glutMouseFunc(MouseButton);
+	glutMotionFunc(MouseMotion);
+	glutPassiveMotionFunc(NULL);
+	glutVisibilityFunc(Visibility);*/
+	glutEntryFunc(NULL);
+	glutSpecialFunc(NULL);
+	glutSpaceballMotionFunc(NULL);
+	glutSpaceballRotateFunc(NULL);
+	glutSpaceballButtonFunc(NULL);
+	glutButtonBoxFunc(NULL);
+	glutDialsFunc(NULL);
+	glutTabletMotionFunc(NULL);
+	glutTabletButtonFunc(NULL);
+	glutMenuStateFunc(NULL);
+	glutTimerFunc(0, NULL, 0);
+
+	// DO NOT SET THE GLUT IDLE FUNCTION HERE !!
+	// glutIdleFunc( NULL );
+	// let glui take care of it in InitGlui( )
+}
+void Framework::InitGlui() {
+
 }
--- Framework.h
+++ Framework.h
@@ -9,15 +9,19 @@
 //#pragma warning(default:C4005)
 class Framework{
 protected:
+	int frameArgc;
+	char ** frameArgv; //Check this later - might be something fishing with pointers
 	//KeyboardState keyboard;
 	//MouseState mouse;
-
 	//std::vector<Updateable*> updateableObjects;
 	//std::vector<Drawable*> drawableObjects;
-
+	void BuildClasses();
+	void InitGlui();
+	void InitGraphics();
 public:
-	Framework(/*int, char ***/);
+	Framework(int, char **);
 	~Framework();
+	void Framework::RestoreDefaults();
 	//virtual void Update(void);
 	//virtual void Draw(void);
 
@@ -25,7 +29,8 @@ public:
 	//virtual void addUpdateable(Updateable*);
 	//virtual void Update(void); //This is the update function
 	//virtual void Draw(void); //This is the render function
-	virtual void Run(void);
+	void virtual Run(void);
+	
 
 };
 
