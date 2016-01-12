5bed20aa903e1d809277eca1b79de3ba05cd42ea
Zosit <zosit123@hotmail.com>
2016-01-12 13:55:32 -0800

    Added SphericalInterpolator

---
+++ SphericalInterpolator.cpp
@@ -0,0 +1,30 @@
+#include "SphericalInterpolator.h"
+
+SphericalInterpolator::SphericalInterpolator()
+{
+
+}
+
+
+SphericalInterpolator::~SphericalInterpolator()
+{
+
+}
+
+//the vectors must be in the order of xyz 000,100,010,001,101,011,110,111
+float* SphericalInterpolator::Build(float vectorInput[8][3], float relativePos[3])
+{
+	float vectorFinal[3];
+	int i;
+	for (i = 0; i < 3; i++) {
+		vectorFinal[i] =	vectorInput[0][i] *  (1 - relativePos[0]) * (1 - relativePos[1]) * (1 - relativePos[2]) +
+							vectorInput[1][i] * (relativePos[0]) * (1 - relativePos[1]) * (1 - relativePos[2]) +
+							vectorInput[2][i] * (1 - relativePos[0]) * relativePos[1] * (1 - relativePos[2]) +
+							vectorInput[3][i] * (1 - relativePos[0]) * (1 - relativePos[1]) * relativePos[2] +
+							vectorInput[4][i] * relativePos[0] * (1 - relativePos[1]) * relativePos[2] +
+							vectorInput[5][i] * (1 - relativePos[0]) * relativePos[1] * relativePos[2] +
+							vectorInput[6][i] * relativePos[0] * relativePos[1] * (1 - relativePos[2]) +
+							vectorInput[7][i] * relativePos[0] * relativePos[1] * relativePos[2];
+	}
+	return vectorFinal;
+}
---
+++ SphericalInterpolator.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "vector3d.h"
+
+class SphericalInterpolator {
+protected:
+	float xyz[3];
+public:
+	SphericalInterpolator();
+	~SphericalInterpolator();
+	float* Build(float [8][3], float[3]);
+};
