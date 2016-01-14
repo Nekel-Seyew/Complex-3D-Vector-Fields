7a7d4eba6372eaa1d00d3b850ec1bec33c951dbe
CorinneBrucks <brucksc@onid.oregonstate.edu>
2016-01-13 20:36:32 -0800

    Finished Barebones Glui Init And Blank Window

--- Framework.cpp
+++ Framework.cpp
@@ -17,6 +17,17 @@ int	DepthCueOn;				// != 0 means to use intensity depth cueing*/
 //If we need too we will make this a singleton
 Framework::Framework(int argc, char ** argv)
 {
+	DepthCueOn = 1;
+	AxesOn = 1;
+	//FOGCOLOR[0] = .0;
+	//FOGCOLOR[1] = .0;
+	//FOGCOLOR[2] = .0;
+	//FOGCOLOR[3] = 1.;
+//	FOGMODE = { GL_LINEAR };
+	//FOGDENSITY = { 0.30f };
+	//FOGSTART = { 1.5 };
+	//FOGEND = { 4. };
+	//MINSCALE = { 0.05f };
 	glutInit(&argc, argv);
 	//frameArgc = argc;
 	//frameArgv = argv;
@@ -59,6 +70,117 @@ void Framework::BuildClasses() {
 }
 void Framework::RestoreDefaults() {
 
+}
+void Framework::Display() {
+
+//	glutSetWindow(MainWindow);
+
+
+	// erase the background:
+
+	glDrawBuffer(GL_BACK);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+
+
+	// specify shading to be flat:
+
+	glShadeModel(GL_FLAT);
+
+
+	// set the viewport to a square centered in the window:
+
+	GLsizei vx = glutGet(GLUT_WINDOW_WIDTH);
+	GLsizei vy = glutGet(GLUT_WINDOW_HEIGHT);
+	GLsizei v = vx < vy ? vx : vy;			// minimum dimension
+	GLint xl = (vx - v) / 2;
+	GLint yb = (vy - v) / 2;
+	glViewport(xl, yb, v, v);
+
+
+	// set the viewing volume:
+	// remember that the Z clipping  values are actually
+	// given as DISTANCES IN FRONT OF THE EYE
+	// USE gluOrtho2D( ) IF YOU ARE DOING 2D !
+	/*
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (WhichProjection == ORTHO)
+		glOrtho(-3., 3., -3., 3., 0.1, 1000.);
+	else
+		gluPerspective(90., 1., 0.1, 1000.);
+
+
+	// place the objects into the scene:
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+
+	// set the eye position, look-at position, and up-vector:
+	// IF DOING 2D, REMOVE THIS -- OTHERWISE ALL YOUR 2D WILL DISAPPEAR !
+
+	gluLookAt(0., 0., 3., 0., 0., 0., 0., 1., 0.);
+
+
+	// translate the objects in the scene:
+	// note the minus sign on the z value
+	// this is to make the appearance of the glui z translate
+	// widget more intuitively match the translate behavior
+	// DO NOT TRANSLATE IN Z IF YOU ARE DOING 2D !
+
+	glTranslatef((GLfloat)TransXYZ[0], (GLfloat)TransXYZ[1], -(GLfloat)TransXYZ[2]);
+
+
+	// rotate the scene:
+	// DO NOT ROTATE (EXCEPT ABOUT Z) IF YOU ARE DOING 2D !
+
+	glRotatef((GLfloat)Yrot, 0., 1., 0.);
+	glRotatef((GLfloat)Xrot, 1., 0., 0.);
+	glMultMatrixf((const GLfloat *)RotMatrix);
+
+
+	// uniformly scale the scene:
+
+	glScalef((GLfloat)Scale, (GLfloat)Scale, (GLfloat)Scale);
+	GLfloat scale2 = 1. + Scale2;		// because glui translation starts at 0.
+	if (scale2 < MINSCALE)
+		scale2 = MINSCALE;
+	glScalef((GLfloat)scale2, (GLfloat)scale2, (GLfloat)scale2);
+
+
+	// set the fog parameters:
+	// DON'T NEED THIS IF DOING 2D !
+/*
+	if (DepthCueOn != 0)
+	{
+		glFogi(GL_FOG_MODE, FOGMODE);
+		glFogfv(GL_FOG_COLOR, FOGCOLOR);
+		glFogf(GL_FOG_DENSITY, FOGDENSITY);
+		glFogf(GL_FOG_START, FOGSTART);
+		glFogf(GL_FOG_END, FOGEND);
+		glEnable(GL_FOG);
+	}
+	else
+	{
+		glDisable(GL_FOG);
+	}
+
+
+	// possibly draw the axes:
+
+	if (AxesOn != 0)
+	{
+//		glCallList(AxesList);
+	}
+	*/
+	
+	// draw the current object:
+
+	//draw the cube:
+	GLdouble size = 2.0;
+	glutWireCube(size);
+	glPointSize(5);
 }
 void Framework::InitGraphics() {
 	// setup the display mode:
@@ -106,8 +228,8 @@ void Framework::InitGraphics() {
 	// IdleFunc -- what to do when nothing else is going on
 
 	glutSetWindow(MainWindow);
-	/*glutDisplayFunc(Display);
-	glutReshapeFunc(Resize);
+	glutDisplayFunc(Framework::Display);
+	/*glutReshapeFunc(Resize);
 	glutKeyboardFunc(Keyboard);
 	glutMouseFunc(MouseButton);
 	glutMotionFunc(MouseMotion);
@@ -138,7 +260,15 @@ void Framework::InitGlui() {
 	Glui->add_statictext((char *)"My Title"); 
 	Glui->add_separator();
 
-	//Glui->add_checkbox("Axes", &AxesOn);
+	Glui->add_checkbox("Axes", &AxesOn);
 
-	//Glui->add_checkbox("Intensity Depth Cue", &DepthCueOn);
+	Glui->add_checkbox("Intensity Depth Cue", &DepthCueOn);
+	// tell glui what graphics window it needs to post a redisplay to:
+
+	Glui->set_main_gfx_window(MainWindow);
+
+
+	// set the graphics window's idle function if needed:
+
+	GLUI_Master.set_glutIdleFunc(NULL);
 }
--- Framework.h
+++ Framework.h
@@ -30,15 +30,35 @@ protected:
 	const char *GLUITITLE = { "User Interface Window" };
 	const int INIT_WINDOW_SIZE = { 600 };
 	const char *WINDOWTITLE = { "OpenGL / GLUT / GLUI Sample -- Corinne Brucks" };
-	
+	// fog parameters:
+	/* static GLfloat FOGCOLOR[4];
+	static GLenum  FOGMODE;
+	static GLfloat FOGDENSITY;
+	static GLfloat FOGSTART;
+	static GLfloat FOGEND; */
 	int MainWindow;
 	int frameArgc;
+	int AxesOn;
+	int DepthCueOn;
+	//static int WhichProjection;
+	//static float MINSCALE;
+	//static GLfloat	RotMatrix[4][4];	// set by glui rotation widget
+	//static float	Scale, Scale2;		// scaling factors
+	//static int	Xmouse, Ymouse;			// mouse values
+	//static float	Xrot, Yrot;			// rotation angles in degrees
+	//static float	TransXYZ[3];		// set by glui translation widgets
+	enum Projections
+	{
+		ORTHO,
+		PERSP
+	};
 	char ** frameArgv; //Check this later - might be something fishy with pointers*/
 	//KeyboardState keyboard;
 	//MouseState mouse;
 	//std::vector<Updateable*> updateableObjects;
 	//std::vector<Drawable*> drawableObjects;
 	void BuildClasses();
+	static void Display();
 	void InitGlui();
 	void InitGraphics();
 public:
@@ -46,6 +66,7 @@ public:
 	Framework(int, char **);
 	~Framework();
 	void Framework::RestoreDefaults();
+
 	//virtual void Update(void);
 	//virtual void Draw(void);
 
